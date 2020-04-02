14902b15bafbef9cf016b1d655f44d4f59c474d4
dmitrynjg <49000073+dmitrynjg@users.noreply.github.com>
2020-04-02 12:43:42 +0300

    first load

---
+++ src/like.js
@@ -0,0 +1,20 @@
+class Like extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: false
+    };
+  }
+
+  render() {
+    const { liked } = this.state;
+    return React.createElement(
+      "div",
+      { onClick: () => this.setState({ liked: !liked }) },
+      `${liked ? "liked" : "unliked"}`
+    );
+  }
+}
+
+const domContainer = document.querySelector("#like");
+ReactDOM.render(React.createElement(Like), domContainer);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/like.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index-bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ["babel-loader"]
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
